c30452fbcb8a9bf66de8ee56fe8081fdea2b6785
mdshepard <mdshepard8@gmail.com>
2018-06-04 10:23:10 -0400

    It's started.

--- todos/src/App.js
+++ todos/src/App.js
@@ -7,15 +7,41 @@ import todoList from './todos.json';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section class="todoapp">
+			<header class="header">
+				<h1>todos</h1>
+				<input class="new-todo" placeholder="What needs to be done?" autofocus/>
+			</header>
+			{/* <!-- This section should be hidden by default and shown when there are todos --> */}
+			<section class="main">
+				<ul class="todo-list">
+					{/* <!-- These are here just to show the structure of the list items --> */}
+					{/* <!-- List items should get the class `editing` when editing and `completed` when marked as completed --> */}
+					<li class="completed">
+						<div class="view">
+							<input class="toggle" type="checkbox" checked/>
+							<label>Taste JavaScript</label>
+							<button class="destroy"></button>
+						</div>
+					</li>
+					<li>
+						<div class="view">
+							<input class="toggle" type="checkbox"/>
+							<label>Buy a unicorn</label>
+							<button class="destroy"></button>
+						</div>
+					</li>
+				</ul>
+			</section>
+			{/* <!-- This footer should hidden by default and shown when there are todos --> */}
+			<footer class="footer">
+				{/* <!-- This should be `0 items left` by default --> */}
+				<span class="todo-count"><strong>0</strong> item(s) left</span>
+				{/* <!-- Remove this if you don't implement routing --> */}
+				{/* <!-- Hidden if no completed items are left ↓ --> */}
+				<button class="clear-completed">Clear completed</button>
+			</footer>
+		</section>
     );
   }
 }
